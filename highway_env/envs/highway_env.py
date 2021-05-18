2125a452bd4806d2756eb33055102e17f16177f8
dgalvezp <68074043+dgalvezp@users.noreply.github.com>
2021-05-18 18:20:34 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -91,7 +91,7 @@ class HighwayEnv(AbstractEnv):
             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1) \
             + self.config["lane_change_reward"]
         reward = utils.lmap(reward,
-                          [self.config["collision_reward"],
+                          [self.config["collision_reward"] + self.config["lane_change_reward"],
                            self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                           [0, 1])
         reward = 0 if not self.vehicle.on_road else reward
